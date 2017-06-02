d40e99192249f1268fb6c35a1cc9217c4a51a05d
BenRoss92 <benjaminjackross@gmail.com>
2017-06-02 16:23:10 +0100

    Add plugin metadata and basic readme

---
+++ global_message.php
@@ -0,0 +1,9 @@
+<?php
+  /*
+  Plugin Name: Global Message
+  Plugin URI: https://github.com/BenRoss92/global_message
+  Description: A plugin to write and save a short message to every front-end page.
+  Author: Ben Ross
+  Author URI: https://github.com/BenRoss92
+  */
+ ?>
