4f9188df2870e7cf14a3b5df79906124df082cca
BenRoss92 <benjaminjackross@gmail.com>
2017-06-03 12:16:32 +0100

    Display 'Global Message' widget with editor in dashboard

--- global_message.php
+++ global_message.php
@@ -6,4 +6,23 @@
   Author: Ben Ross
   Author URI: https://github.com/BenRoss92
   */
- ?>
+
+ function add_widget_editor() {
+  $default_content = '';
+  $editor_id = 'global-message-editor';
+  $editor_settings = array(
+    'media_buttons' => FALSE,
+    'textarea_name' => 'editor_text',
+    'textarea_rows' => 5
+  );
+  wp_editor($default_content, $editor_id, $editor_settings);
+ }
+
+ function add_widget_to_dashboard() {
+   wp_add_dashboard_widget('global-message-widget',
+   'Global Message', 'add_widget_editor');
+ }
+
+ add_action('wp_dashboard_setup', 'add_widget_to_dashboard');
+
+?>
