5ebc8f472a85d201405b2a885f0cad0c4169b721
BenRoss92 <benjaminjackross@gmail.com>
2017-06-03 14:08:10 +0100

    Change built-in editor to form - Form is currently static; performs no action

--- global_message.php
+++ global_message.php
@@ -7,20 +7,21 @@
   Author URI: https://github.com/BenRoss92
   */
 
- function add_widget_editor() {
-  $default_content = '';
-  $editor_id = 'global-message-editor';
-  $editor_settings = array(
-    'media_buttons' => FALSE,
-    'textarea_name' => 'editor_text',
-    'textarea_rows' => 5
-  );
-  wp_editor($default_content, $editor_id, $editor_settings);
+ function add_widget_form() {
+   ?>
+   <form>
+     <h3>Message:</h3>
+     <div class="textarea-wrap">
+        <textarea name="global-message-text" rows="3" cols="15"></textarea>
+     </div>
+     <?php submit_button('Update Message'); ?>
+   </form>
+   <?php
  }
 
  function add_widget_to_dashboard() {
    wp_add_dashboard_widget('global-message-widget',
-   'Global Message', 'add_widget_editor');
+   'Global Message', 'add_widget_form');
  }
 
  add_action('wp_dashboard_setup', 'add_widget_to_dashboard');
