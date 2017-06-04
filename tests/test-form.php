6af4adff546bee90d627e5b8f79c85dac1b44a33
BenRoss92 <benjaminjackross@gmail.com>
2017-06-04 13:48:51 +0100

    Add and configure WP-CLI for unit tests

---
+++ tests/test-form.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Class Global_Message_Form_Tests
+ *
+ * @package Global_message
+ */
+
+class Global_Message_Form_Tests extends WP_UnitTestCase {
+
+	function test_add_widget_form() {
+		$this->assertTrue(true);
+	}
+
+}
