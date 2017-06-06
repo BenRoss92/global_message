8237fd2a805427cb9688e9282bf8ead15bc9e85c
BenRoss92 <benjaminjackross@gmail.com>
2017-06-06 14:26:11 +0100

    Change form to built-in wordpress editor - Characters such as quotes not displaying properly

--- global_message.php
+++ global_message.php
@@ -7,43 +7,45 @@
   Author URI: https://github.com/BenRoss92
   */
 
-  function add_widget_form() {
+  function add_widget_editor() {
+    $default_content = get_option('global_message');
+    $editor_id = 'global-message-editor';
+    $editor_settings = array(
+      'media_buttons' => FALSE,
+      'textarea_name' => 'editor_text',
+      'textarea_rows' => 5
+    );
     ?>
       <form action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post">
-        <h3>Message:</h3>
-        <div class="textarea-wrap">
-          <textarea name="global-message-text"
-          placeholder="e.g. Half-price sale now on!"
-          rows="3" cols="15"><?php echo get_option('global_message'); ?></textarea>
-        </div>
+        <?php wp_editor($default_content, $editor_id, $editor_settings); ?>
         <input type="hidden" name="action" value="global_message_form">
         <?php submit_button('Update Message'); ?>
       </form>
     <?php
   }
 
- add_action('wp_dashboard_setup', 'add_widget_to_dashboard');
+  add_action('wp_dashboard_setup', 'add_widget_to_dashboard');
 
- function add_widget_to_dashboard() {
-   wp_add_dashboard_widget('global-message-widget',
-   'Global Message', 'add_widget_form');
- }
+  function add_widget_to_dashboard() {
+    wp_add_dashboard_widget('global-message-widget',
+    'Global Message', 'add_widget_editor');
+  }
 
- add_action('admin_post_global_message_form', 'add_message_to_database');
+  add_action('admin_post_global_message_form', 'add_message_to_database');
 
- function add_message_to_database() {
-   $message_to_add = $_POST['global-message-text'];
-   update_option('global_message', $message_to_add);
+  function add_message_to_database() {
+    $message_to_add = $_POST['editor_text'];
+    update_option('global_message', $message_to_add);
 
-   wp_safe_redirect('http://localhost/~Ben/wordpress/wp-admin/');
-   exit;
- }
+    wp_safe_redirect('http://localhost/~Ben/wordpress/wp-admin/');
+    exit;
+  }
 
- add_filter('the_content', 'add_mesage_to_content');
+  add_filter('the_content', 'add_mesage_to_content');
 
- function add_mesage_to_content($content) {
-   $message_to_add = get_option('global_message');
-   return $message_to_add . $content;
- }
+  function add_mesage_to_content($content) {
+    $message_to_add = get_option('global_message');
+    return $message_to_add . $content;
+  }
 
 ?>
